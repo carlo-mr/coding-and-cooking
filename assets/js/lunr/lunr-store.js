8fb16c52d90ee7b9869939871788ed7c1708d728
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-03 10:47:51 +0000

    Deploy carlo-mr/coding-and-cooking to github.com/carlo-mr/coding-and-cooking.git:gh-pages

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Vegan Mousse au Chocolat",
+        "excerpt":"Today I had a small dilemma. I could not decide if I want to continue programming to process on the coding-related blog series or if I should finally add a cooking related post to my site. As so often my wife helped me decide. She wished to try the vegan...","categories": ["cooking"],
+        "tags": ["dessert","vegan"],
+        "url": "/cooking/chick-peas-salad/",
+        "teaser":"/assets/images/chick-peas-salad/ingredients-mousse.jpg"},{
+        "title": "I bought a cow",
+        "excerpt":"Ok not a complete cow but some parts. The idea is called cow sharing or crowd butching/butchering. I was made aware of that concept by a tweet of @andreasdotorg – who I follow since I heard a german podcast featuring him talking about the history and process of brewing beer...","categories": ["cooking"],
+        "tags": ["beef"],
+        "url": "/cooking/buyacow/",
+        "teaser":"/assets/images/buyacow/cow.png"},{
+        "title": "The Duties of a Software Architect",
+        "excerpt":"Last week I had the opportunity to attended the workshop “Mastering Software Architectures” by Gernot Starke and Peter Hruschka. If you ever have the chance to have a training with the two you should take the opportunity! In this post I want to recap the 6 duties that form the...","categories": ["coding"],
+        "tags": ["architecture"],
+        "url": "/coding/duties-of-a-software-architect/",
+        "teaser":"/assets/images/architect-duties/teaser.jpg"},{
+        "title": "A simple Web API using Claudia.js, AWS and DynamoDB",
+        "excerpt":"Abstract Recently I played around with Claudia.js and AWS Lambda. And I thought that could be a good technical stack for the “Illegal or frowned upon” api. So I gave the Claudia.js API Builder a chance – and it was so easy that I want to quickly write this up...","categories": ["coding"],
+        "tags": [],
+        "url": "/coding/simple-web-api-using-claudiajs/",
+        "teaser":"/assets/images/simple-web-api/teaser.jpg"}]
