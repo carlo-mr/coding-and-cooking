b7c016541c44c1be87c62b96309fee11b796ba16
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-11 06:44:01 +0000

    Deploy carlo-mr/coding-and-cooking to github.com/carlo-mr/coding-and-cooking.git:gh-pages

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "Vegan Mousse au Chocolat",
         "excerpt":"Today I had a small dilemma. I could not decide if I want to continue programming to process on the coding-related blog series or if I should finally add a cooking related post to my site. As so often my wife helped me decide. She wished to try the vegan...","categories": ["cooking"],
         "tags": ["dessert","vegan"],
-        "url": "https://carlo-mr.github.io/coding-and-cooking/cooking/vegan-mousse-au-chocolat/",
-        "teaser":"https://carlo-mr.github.io/coding-and-cooking/assets/images/vegan-mousse/ingredients-mousse.jpg"},{
+        "url": "http://coding-and-cooking.com/cooking/vegan-mousse-au-chocolat/",
+        "teaser":"http://coding-and-cooking.com/assets/images/vegan-mousse/ingredients-mousse.jpg"},{
         "title": "I bought a cow",
         "excerpt":"Ok not a complete cow but some parts. The idea is called cow sharing or crowd butching/butchering. I was made aware of that concept by a tweet of @andreasdotorg – who I follow since I heard a german podcast featuring him talking about the history and process of brewing beer...","categories": ["cooking"],
         "tags": ["beef"],
-        "url": "https://carlo-mr.github.io/coding-and-cooking/cooking/buyacow/",
-        "teaser":"https://carlo-mr.github.io/coding-and-cooking/assets/images/buyacow/cow.png"},{
+        "url": "http://coding-and-cooking.com/cooking/buyacow/",
+        "teaser":"http://coding-and-cooking.com/assets/images/buyacow/cow.png"},{
         "title": "The Duties of a Software Architect",
         "excerpt":"Last week I had the opportunity to attended the workshop “Mastering Software Architectures” by Gernot Starke and Peter Hruschka. If you ever have the chance to have a training with the two you should take the opportunity! In this post I want to recap the 6 duties that form the...","categories": ["coding"],
         "tags": ["architecture"],
-        "url": "https://carlo-mr.github.io/coding-and-cooking/coding/duties-of-a-software-architect/",
-        "teaser":"https://carlo-mr.github.io/coding-and-cooking/assets/images/architect-duties/teaser.jpg"},{
+        "url": "http://coding-and-cooking.com/coding/duties-of-a-software-architect/",
+        "teaser":"http://coding-and-cooking.com/assets/images/architect-duties/teaser.jpg"},{
         "title": "A simple Web API using Claudia.js, AWS and DynamoDB",
         "excerpt":"Abstract Recently I played around with Claudia.js and AWS Lambda. And I thought that could be a good technical stack for the “Illegal or frowned upon” api. So I gave the Claudia.js API Builder a chance – and it was so easy that I want to quickly write this up...","categories": ["coding"],
         "tags": [],
-        "url": "https://carlo-mr.github.io/coding-and-cooking/coding/simple-web-api-using-claudiajs/",
-        "teaser":"https://carlo-mr.github.io/coding-and-cooking/assets/images/simple-web-api/teaser.jpg"},{
+        "url": "http://coding-and-cooking.com/coding/simple-web-api-using-claudiajs/",
+        "teaser":"http://coding-and-cooking.com/assets/images/simple-web-api/teaser.jpg"},{
         "title": "FriendLib - Intro to my current side project",
         "excerpt":"Since a August 2018 I’m working on a new side project called FriendLib. The basic idea about the app is that you can check which books your friends own and instead of buying the books you ask you friends if you can lend the book. The name is a portmanteau...","categories": ["coding"],
         "tags": [],
-        "url": "https://carlo-mr.github.io/coding-and-cooking/coding/friendlib-intro/",
-        "teaser":"https://carlo-mr.github.io/coding-and-cooking/assets/images/friendlib-intro/teaser.jpg"}]
+        "url": "http://coding-and-cooking.com/coding/friendlib-intro/",
+        "teaser":"http://coding-and-cooking.com/assets/images/friendlib-intro/teaser.jpg"}]
